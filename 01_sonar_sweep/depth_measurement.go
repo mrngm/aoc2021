051775ba884ca1f54d16cedb1cf93d3b89968db3
Gerdriaan Mulder <hello@moeilijklastig.nl>
2021-12-04 19:31:44 +0100

    Day 1: Sonar Sweep (puzzle 2/2)

--- 01_sonar_sweep/depth_measurement.go
+++ 01_sonar_sweep/depth_measurement.go
@@ -10,6 +10,7 @@ import (
 
 var (
 	inputFile = flag.String("input", "input", "The input file")
+	sliding   = flag.Bool("sliding", false, "Use sliding window method")
 )
 
 func SweepDepthIncrease(inputs []int) int {
@@ -23,11 +24,29 @@ func SweepDepthIncrease(inputs []int) int {
 		}
 		if d > lastInput {
 			ret++
-			lastInput = d
 		}
 		lastInput = d
 	}
 	return ret
+}
+
+func SweepDepthIncreaseWithSlidingWindow(inputs []int) (int, int) {
+	ret := 0
+	nWindows := 0
+
+	for n := 0; n+3 < len(inputs); n++ {
+		nWindows++
+	}
+
+	for w := 0; w < nWindows; w++ {
+		thisWindow := inputs[w+2] + inputs[w+1] + inputs[w]
+		nextWindow := inputs[w+3] + inputs[w+2] + inputs[w+1]
+		if nextWindow > thisWindow {
+			ret++
+		}
+	}
+
+	return ret, nWindows + 1
 }
 
 func main() {
@@ -46,6 +65,11 @@ func main() {
 		inputInts[n] = theInt
 	}
 
-	out := SweepDepthIncrease(inputInts)
-	log.Printf("After %d measurements, we found %d measurements that are larger than the previous measurements", len(inputInts), out)
+	if *sliding {
+		out, nWindows := SweepDepthIncreaseWithSlidingWindow(inputInts)
+		log.Printf("After %d measurements with %d sliding windows, we found %d measurements that are larger than the previous measurements", len(inputInts), nWindows, out)
+	} else {
+		out := SweepDepthIncrease(inputInts)
+		log.Printf("After %d measurements, we found %d measurements that are larger than the previous measurements", len(inputInts), out)
+	}
 }
--- 01_sonar_sweep/depth_measurement_test.go
+++ 01_sonar_sweep/depth_measurement_test.go
@@ -5,28 +5,26 @@ import (
 )
 
 const (
-	testOutput = 7
+	testOutput              = 7
+	testOutputSlidingWindow = 5
 )
 
 var (
-	testInput = []int{
-		199,
-		200,
-		208,
-		210,
-		200,
-		207,
-		240,
-		269,
-		260,
-		263,
-	}
+	testInput = []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
 )
 
 func TestSweepDepthIncreases(t *testing.T) {
 	out := SweepDepthIncrease(testInput)
 	if out != testOutput {
-		t.Fatalf("out: %v, expected: %v", out, testOutput)
+		t.Fatalf("%s: out: %v, expected: %v", t.Name(), out, testOutput)
 	}
-	t.Logf("out: %v, expected: %v", out, testOutput)
+	t.Logf("%s: out: %v, expected: %v", t.Name(), out, testOutput)
+}
+
+func TestSweepDepthIncreaseWithSlidingWindow(t *testing.T) {
+	out, _ := SweepDepthIncreaseWithSlidingWindow(testInput)
+	if out != testOutputSlidingWindow {
+		t.Fatalf("%s: out: %v, expected: %v", t.Name(), out, testOutputSlidingWindow)
+	}
+	t.Logf("%s: out: %v, expected: %v", t.Name(), out, testOutputSlidingWindow)
 }
